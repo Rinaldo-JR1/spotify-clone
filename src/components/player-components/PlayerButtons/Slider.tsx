0df0f676ace8051708757ca0d00567cf31e1a004
rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2023-07-27 11:59:53 -0300

    Versão interativa

--- src/components/player-components/PlayerButtons/Slider.tsx
+++ src/components/player-components/PlayerButtons/Slider.tsx
@@ -7,6 +7,7 @@ export const Slider = () => {
             <div className={"h-1 rounded-full w-96 bg-zinc-600"}>
                 <div className={"bg-zinc-200 w-10 h-1 rounded-full"}></div>
             </div>
+            
             <span className={"text-xs text-zinc-400"}>3:08</span>
         </>
 
